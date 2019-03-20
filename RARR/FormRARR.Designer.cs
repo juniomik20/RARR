dc4f054108acf77e6ed4978a97ce84aedbb3ce14
Mike <mj.egea@sarria.salesians.cat>
2019-03-20 19:25:38 +0100

    ConnectionClass EntityFramework
    configure global appconfig, create a object EntityDB

--- RARR/FormRARR.Designer.cs
+++ RARR/FormRARR.Designer.cs
@@ -28,23 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRarr));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(356, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "label1";
             // 
             // FormRarr
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Location = new System.Drawing.Point(0, 0);
             this.Name = "FormRarr";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormRarr_Load);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- RARR/FormRARR.cs
+++ RARR/FormRARR.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConnectionEntityClass;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
@@ -13,10 +14,21 @@ namespace RARR
 {
     public partial class FormRarr : PlantillaPrincipalStarWars.PlantillaForm
     {
+        RepublicRebelEntities entityDB = new RepublicRebelEntities();
+
         public FormRarr()
         {
+            try
+            {
             this.TittleScreen.Text = "Factory X-Wings";
             InitializeComponent();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error");
+            }
+          
         }
 
         private void FormRarr_Load(object sender, EventArgs e)
@@ -26,8 +38,36 @@ namespace RARR
 
         private void Actualizar()
         {
+            try { 
             this.Username = ConfigurationManager.AppSettings["UserName"].ToString();
             this.IconUser = ConfigurationManager.AppSettings["Icon"].ToString();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error , has entrado a la aplicacion sin iniciar sesion ");
+                Application.Exit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cadena;
+                var onlineOrders =
+                    from order in entityDB.FactoryUsers
+                    select new
+                    {
+                        SalesOrderID = order.idUser,
+                        OrderDate = order.UserName,
+                        SalesOrderNumber = order.idUserType
+                    };
+
+                foreach (var onlineOrder in onlineOrders)
+                {
+                label1.Text = "Order ID: "+ onlineOrder.SalesOrderID +" Order date: "+ onlineOrder.OrderDate + "Order number: "+ onlineOrder.SalesOrderNumber;
+                }
+            
+
         }
     }
 }
